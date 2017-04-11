4f2c7ddc321f0d54a0de8854ff73a745fe4e08db
Anton Kovsharov <antonkovsharov@gmail.com>
2017-04-11 18:46:57 +0300

    fix

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -21,17 +21,17 @@ const chooseElement = function(x, y) {
     }
   }
 
-  sx = [0.149, 0.207, 0.264, 0.320, 0.389, 0.435, 0.493, 0.550, 0.607, 0.664, 0.721, 0.779, 0.835, 0.893, 0.950]
-  sy = [0.873, 0.915, 0.957]
+  xs = [0.149, 0.207, 0.264, 0.320, 0.389, 0.435, 0.493, 0.550, 0.607, 0.664, 0.721, 0.779, 0.835, 0.893, 0.950]
+  ys = [0.873, 0.915, 0.957]
   elements = [
     ['Церий', 'Празеодим', 'Неодим', 'Прометий', 'Самарий', 'Европий', 'Галидоний', 'Тербий', 'Диспрозий',
     'Гольмий', 'Эрбий', 'Тулий', 'Иттербий', 'Лютеций'],
     ['Торий', 'Протактиний', 'Уран', 'Нептуний', 'Плутоний', 'Америций', 'Кюрий', 'Берклий', 'Калифорний',
     'Эйнштейний', 'Фермий', 'Менделевий', 'Нобелий', 'Лоуренсий']
   ]
-  for (let i = 1; i < sx.length; i++) {
-    for (let j = 1; j < sy.length; j++) {
-      if (x >= sx[i - 1] && x < sx[i] && y >= sy[j - 1] && y < sy[j]) {
+  for (let i = 1; i < xs.length; i++) {
+    for (let j = 1; j < ys.length; j++) {
+      if (x >= xs[i - 1] && x < xs[i] && y >= ys[j - 1] && y < ys[j]) {
         return elements[j - 1][i - 1]
       }
     }
@@ -40,24 +40,47 @@ const chooseElement = function(x, y) {
   return '*'
 }
 
+chooseMultipleState = false;
+chosenElems = new Set()
+
 $(document).ready(function(){
   $('.table').bind('click', function (ev) {
-      const $div = $(ev.target);
+    const $div = $(ev.target);
 
-      const offset = $div.offset();
-      let x = ev.clientX - offset.left;
-      let y = ev.clientY - offset.top;
-      ratio = 3543 / 2657
-      console.log($(window).height())
-      height = $(window).height()
-      width = height * ratio
-      x /= width
-      y /= height
+    const offset = $div.offset();
+    let x = ev.clientX - offset.left;
+    let y = ev.clientY - offset.top;
+    ratio = 3543 / 2657
+    console.log($(window).height())
+    height = $(window).height()
+    width = height * ratio
+    x /= width
+    y /= height
 
-      elem = chooseElement(x, y)
-      if (elem != "*") {
+    elem = chooseElement(x, y)
+    if (elem != "*") {
+      if (chooseMultipleState) {
+        if (chosenElems.has(elem)) {
+          chosenElems.delete(elem)
+        } else {
+          chosenElems.add(elem)
+        }
+        console.log(chosenElems)
+      } else {
         window.location.href = "/show?element=" + elem;
       }
-      console.log('x: ' + x + ', y: ' + y + ', elem' + chooseElement(x, y));
+    }
+    console.log('x: ' + x + ', y: ' + y + ', elem' + chooseElement(x, y));
   });
+  $('.button').click(function() {
+    if (chooseMultipleState == false) {
+      chooseMultipleState = true
+      chosenElems = new Set()
+      $('.button').html('Открыть')
+    } else {
+      window.location.href = "/show?element=" + Array.from(chosenElems).join()
+      chooseMultipleState = false
+      $('.button').html('Выбрать несколько')
+    }
+  })
 });
